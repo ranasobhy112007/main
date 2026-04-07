d126b3f23302deed021cfe2fb6f7176ea3c516c8
ranasobhy112007 <ranasobhy112007@gmail.com>
2026-04-07 13:18:27 +0200

    Update main.cpp.cpp

--- main.cpp/main.cpp.cpp
+++ main.cpp/main.cpp.cpp
@@ -75,3 +75,56 @@ int main()
 {
 	return 0;
 }
+
+void SearchByDate(MoodEntry moods[], int moodcount) {
+	int day, month, year;
+	bool found = false;
+
+	cout << "Enter the day:";
+	cin >> day;
+	cout << endl << "Enter the month:";
+	cin >> month;
+	cout << endl << "Enter the year:";
+	cin >> year;
+	for (int i = 0; i < moodcount; i++)
+		if (moods[i].day == day && moods[i].month == month && moods[i].year == year) {
+			cout << "Date:" << moods[i].day << "/" << moods[i].month << "/" << moods[i].year << endl
+				<< moods[i].moodtype << endl << moods[i].moodlevel << endl << moods[i].note << endl 
+				<< "____________________________________________________________";
+			found = true;
+		}
+	if (!found)
+		cout << "No mood found on this date." << endl;
+}
+
+
+void SearchByType(MoodEntry moods[], int moodcount) {
+	string type;
+	bool found = false;
+
+	cout << "Enter the type of mood:";
+	cin >> type;
+
+	  for (int i = 0; i < moodcount; i++)
+		  if (moods[i].type == type) {
+			cout << "Date:" << moods[i].day << "/" << moods[i].month << "/" << moods[i].year << endl
+				<< moods[i].moodtype << endl << moods[i].moodlevel << endl << moods[i].note  << endl 
+				<< "____________________________________________________________";
+			found = true;
+		  }
+	if (!found)
+		cout << "No mood found on with this type." << endl;
+}
+
+
+void SearchMood() {
+	int choise;
+	cout << "ENTER 1 to search by date." << endl << "ENTER 2 to search by mood type";
+	cin >> choise;
+	if (choise == 1)
+		SearchByDate( moods , moodcount);
+	else if (choise == 2)
+		SearchByType( moods , moodcount);
+	else
+		cout << "invalid choise!" << endl;
+}
